0f2a366957d2e9a918cf619452f3cd55db915b84
Emma Hogan <emma.hogan@metoffice.gov.uk>
2022-08-05 15:27:27 +0100

    #1: Use RTD theme, update copyright, ensure documentation builds correctly

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -18,7 +18,7 @@
 # -- Project information -----------------------------------------------------
 
 project = 'Climate Assessment Workflow (CAW)'
-copyright = '2022, Emma Hogan'
+copyright = 'Crown Copyright 2022, the Met Office'
 author = 'Emma Hogan'
 
 
@@ -44,7 +44,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
--- doc/source/index.rst
+++ doc/source/index.rst
@@ -3,18 +3,16 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to Climate Assessment Workflow (CAW)'s documentation!
-=============================================================
+Welcome to the Climate Assessment Workflow (CAW)'s documentation!
+=================================================================
 
 .. toctree::
-   :maxdepth: 2
-   :caption: Contents:
+   :maxdepth: 1
+   :caption: Contents
 
 
 
 Indices and tables
-==================
+------------------
 
 * :ref:`genindex`
-* :ref:`modindex`
-* :ref:`search`
