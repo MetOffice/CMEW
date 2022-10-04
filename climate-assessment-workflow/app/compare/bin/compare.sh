4451762210289a4e25d5b14e3f0dba6eb03ff0f1
Emma Hogan <ehogan@users.noreply.github.com>
2022-10-04 14:12:26 +0100

    #14: Add test infrastructure
    * #13: Add compare task that runs only when TEST=true, update data ROOTPATHs
    
    * #13: Changes in response to review: uppercase usage in comment
    
    * #13: Changes in response to review: add name of cylc file as comment at the top of the file
    
    * #13: Changes in response to review: move test.cylc to inc directory
    
    * #13: Add documentation for compare task
    
    * #13: Use app conf files for compare task
    
    * #13: Revert changes to compare.sh
    
    * #13: Remove unnecessary environment variables

---
+++ climate-assessment-workflow/app/compare/bin/compare.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# USAGE compare.sh
+#
+# ENVIRONMENT
+#   OUTPUT_DIR    The full path to the output directory for the 'process' task
+#   METRIC_DIR    The name of the directory in OUTPUT_DIR containing the
+#                 METRIC_FILES
+#   METRIC_FILES  The files expected to be produced by the process task for the
+#                 metric
+
+# Send the output from 'set -x' to 'stdout' rather than 'stderr'.
+BASH_XTRACEFD=1
+set -eux
+
+# Ensure the files produced by the 'process' task for the metric match the
+# expected files.
+for metric_file in ${METRIC_FILES}
+    do
+        OUTPUT_METRIC_PATH="${OUTPUT_DIR}/${METRIC_DIR}*/${metric_file}"
+        if [ ! -f ${OUTPUT_METRIC_PATH} ]; then
+            echo "${OUTPUT_METRIC_PATH} does not exist"
+            exit 1
+        fi
+    done
