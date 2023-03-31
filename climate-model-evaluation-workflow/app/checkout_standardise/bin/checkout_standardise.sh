5f42a7d9787f256422e0299c7f9a4abe3c5d6d72
Theo Geddes <theodore.geddes@metoffice.gov.uk>
2023-03-31 10:04:51 +0100

    #71: add -c to git command

--- climate-model-evaluation-workflow/app/checkout_standardise/bin/checkout_standardise.sh
+++ climate-model-evaluation-workflow/app/checkout_standardise/bin/checkout_standardise.sh
@@ -9,4 +9,4 @@ if [[ -d ${CDDS_DIR} ]]; then
 fi
 
 # Checkout the specified branch of the CMEW using -q
-git clone -q -b "${BRANCH}" "${CDDS_URL}" "${CDDS_DIR}"
+git clone -c advice.detachedHead=false -q -b "${BRANCH}" "${CDDS_URL}" "${CDDS_DIR}"
