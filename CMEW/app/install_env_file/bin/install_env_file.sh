97092ba604dd93abd0a21779848258819d96c287
Josh Rackham <josh.rackham@metoffice.gov.uk>
2024-01-12 11:02:13 +0000

    #80: Use CYLC_WORKFLOW_SHARE environment variable

--- CMEW/app/install_env_file/bin/install_env_file.sh
+++ CMEW/app/install_env_file/bin/install_env_file.sh
@@ -4,7 +4,7 @@ BASH_XTRACEFD=1
 set -eux
 
 SOURCE_PATH="${CYLC_WORKFLOW_RUN_DIR}/site/${SITE}-process-env"
-TARGET_DIR="${CYLC_WORKFLOW_RUN_DIR}/share/bin"
+TARGET_DIR="${CYLC_WORKFLOW_SHARE_DIR}/bin"
 ENV_FILE="cmew-process-env"
 
 # Create the 'bin' directory in the installed workflow.
