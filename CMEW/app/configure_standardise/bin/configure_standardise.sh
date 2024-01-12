3c9cde13006d7ac5a645cf756e0ee0e07548911e
Josh Rackham <josh.rackham@metoffice.gov.uk>
2024-01-12 15:12:49 +0000

    #105: Created dummy configure_standardise.sh script

---
+++ CMEW/app/configure_standardise/bin/configure_standardise.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Send the output from 'set -x' to 'stdout' rather than 'stderr'.
+BASH_XTRACEFD=1
+set -eux
+
+echo "Running configure_standardise"
+echo "Get request.json file"
+echo "Get variable list"
