c9e3163d5d6623d86712540ccc14207684181364
Ed Gillett <146008263+mo-gill@users.noreply.github.com>
2024-01-15 13:30:46 +0000

    #106: corrected two variables to ENV_PROCESS_FILE and SOURCE_PROCESS_PATH

--- CMEW/app/install_env_file/bin/install_env_file.sh
+++ CMEW/app/install_env_file/bin/install_env_file.sh
@@ -4,17 +4,17 @@ BASH_XTRACEFD=1
 set -eux
 
 # Paths for site-specific environment scripts
-SOURCE_PATH="${CYLC_WORKFLOW_RUN_DIR}/site/${SITE}-process-env"
+SOURCE_PROCESS_PATH="${CYLC_WORKFLOW_RUN_DIR}/site/${SITE}-process-env"
 SOURCE_STANDARDISE_PATH="${CYLC_WORKFLOW_RUN_DIR}/site/${SITE}-standardise-env"
 # Target directory for installation of the environment scripts
 TARGET_DIR="${CYLC_WORKFLOW_RUN_DIR}/share/bin"
 # Names of the environment scripts
-ENV_FILE="cmew-env"
+ENV_PROCESS_FILE="cmew-env"
 ENV_STANDARDISE_FILE="cmew-standardise-env"
 
 # Create the 'bin' directory in the installed workflow.
 mkdir "${TARGET_DIR}"
 
 # Copy the environment scripts to the 'bin' directory.
-cp "${SOURCE_PATH}" "${TARGET_DIR}/${ENV_FILE}"
+cp "${SOURCE_PROCESS_PATH}" "${TARGET_DIR}/${ENV_PROCESS_FILE}"
 cp "${SOURCE_STANDARDISE_PATH}" "${TARGET_DIR}/${ENV_STANDARDISE_FILE}"
