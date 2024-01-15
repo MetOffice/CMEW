0d114eb32973fdfdccd3b657ab5f46b0526070ec
Ed Gillett <146008263+mo-gill@users.noreply.github.com>
2024-01-15 11:18:58 +0000

    #106: resolving requested changes re: PR#123

--- CMEW/app/install_env_file/bin/install_env_file.sh
+++ CMEW/app/install_env_file/bin/install_env_file.sh
@@ -3,18 +3,18 @@
 BASH_XTRACEFD=1
 set -eux
 
-# Paths for site-specific environments scripts
+# Paths for site-specific environment scripts
 SOURCE_PATH="${CYLC_WORKFLOW_RUN_DIR}/site/${SITE}-process-env"
 SOURCE_STANDARDISE_PATH="${CYLC_WORKFLOW_RUN_DIR}/site/${SITE}-standardise-env"
-# Target directory for installation of the environments scripts
+# Target directory for installation of the environment scripts
 TARGET_DIR="${CYLC_WORKFLOW_RUN_DIR}/share/bin"
-# Names of the environments scripts
+# Names of the environment scripts
 ENV_FILE="cmew-env"
 ENV_STANDARDISE_FILE="cmew-standardise-env"
 
 # Create the 'bin' directory in the installed workflow.
 mkdir "${TARGET_DIR}"
 
-# Copy the environments scripts to the 'bin' directory.
+# Copy the environment scripts to the 'bin' directory.
 cp "${SOURCE_PATH}" "${TARGET_DIR}/${ENV_FILE}"
 cp "${SOURCE_STANDARDISE_PATH}" "${TARGET_DIR}/${ENV_STANDARDISE_FILE}"
