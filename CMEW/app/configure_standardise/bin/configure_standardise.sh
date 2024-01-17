279c137fdb4e5a2cd01bcfb7645414eb5a770fff
dasha-shchep <28752245+dasha-shchep@users.noreply.github.com>
2024-01-17 16:56:36 +0000

    #60: configure_standardise.py now writes variables file to share/etc dir

--- CMEW/app/configure_standardise/bin/configure_standardise.sh
+++ CMEW/app/configure_standardise/bin/configure_standardise.sh
@@ -3,8 +3,9 @@
 BASH_XTRACEFD=1
 set -eux
 
+mkdir -p ${CYLC_WORKFLOW_SHARE_DIR}/etc
+
 echo "Running configure_standardise"
 echo "Get request.json file"
 echo "Get variable list"
-
 cmew-process-env configure_standardise.py
