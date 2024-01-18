0102640707c87f71a21efb538835caf1b4657bb9
dasha-shchep <28752245+dasha-shchep@users.noreply.github.com>
2024-01-18 16:58:12 +0000

    #60: Added unit test for configure_standardise script

--- CMEW/app/configure_standardise/bin/configure_standardise.py
+++ CMEW/app/configure_standardise/bin/configure_standardise.py
@@ -17,14 +17,13 @@ def extract_variables():
 
 def write_variables(variables: str):
     SHARE_DIR = os.environ["CYLC_WORKFLOW_SHARE_DIR"]
-    with open(SHARE_DIR + "/etc/variables_test.txt", mode="w+") as file:
+    with open(SHARE_DIR + "/etc/variables.txt", mode="w+") as file:
         file.write(variables)
     return None
 
 
 def main():
     variables = extract_variables()
-    print(variables)
     write_variables(variables=variables)
     return None
 
