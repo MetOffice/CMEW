9238b2f04cfc3f2bd910fc479acfb30e413a3d27
nabilbrice <mo-nabilbrice@users.noreply.github.com>
2024-01-18 13:57:21 +0000

    #104: fixed format issues

--- CMEW/app/configure_standardise/bin/configure_standardise.py
+++ CMEW/app/configure_standardise/bin/configure_standardise.py
@@ -7,23 +7,27 @@ Generates the variables.txt file from the ESMValTool recipe.
 import os
 from pathlib import Path
 
+
 def extract_request() -> str:
     configure_standardise_path = Path(__file__).parent.parent
     with open(configure_standardise_path / "mock_data/request.json") as file:
         request = file.read()
     return request
 
+
 def write_request(request: str):
-    SHARE_DIR = os.environ["CYLC_WORKFLOW_SHARE_DIR"]
-    with open(SHARE_DIR + "/etc/request.json", mode="w+") as file:
+    share_dir = os.environ["CYLC_WORKFLOW_SHARE_DIR"]
+    with open(share_dir + "/etc/request.json", mode="w+") as file:
         file.write(request)
     return None
+
 
 def main():
     request = extract_request()
     print(request)
     write_request(request=request)
     return None
+
 
 if __name__ == "__main__":
     main()
