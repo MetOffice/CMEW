bd814476ec37cd9b47ee8327138caf2c3faf644d
nabilbrice <mo-nabilbrice@users.noreply.github.com>
2024-01-18 13:14:18 +0000

    #104: moved code that copies the request.json file into configure_standardise.py

---
+++ CMEW/app/configure_standardise/bin/configure_standardise.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+"""
+Generates the request.json file from the ESMValTool recipe.
+
+Generates the variables.txt file from the ESMValTool recipe.
+"""
+import os
+from pathlib import Path
+
+def extract_request() -> str:
+    configure_standardise_path = Path(__file__).parent.parent
+    with open(configure_standardise_path / "mock_data/request.json") as file:
+        request = file.read()
+    return request
+
+def write_variables(request: str):
+    SHARE_DIR = os.environ["CYLC_WORKFLOW_SHARE_DIR"]
+    with open(SHARE_DIR + "/etc/request.json", mode="w+") as file:
+        file.write(request)
+    return None
+
+def main():
+    request = extract_request()
+    print(request)
+    write_variables(request=request)
+    return None
+
+if __name__ == "__main__":
+    main()
--- CMEW/app/configure_standardise/bin/configure_standardise.sh
+++ CMEW/app/configure_standardise/bin/configure_standardise.sh
@@ -4,13 +4,6 @@ BASH_XTRACEFD=1
 set -eux
 
 echo "Running configure_standardise"
-
 echo "Get request.json file"
-# The directory of the hardcoded request.json file
-SOURCE_PATH="${CYLC_WORKFLOW_RUN_DIR}/app/configure_standardise/mock_data"
-TARGET_DIR="${CYLC_WORKFLOW_SHARE_DIR}/etc"
-# Copy the request.json file to the 'share/etc' directory
-mkdir -p ${TARGET_DIR}
-cp "${SOURCE_PATH}/request.json" "${TARGET_DIR}/request.json"
-
+cmew-process-env configure_standardise.py
 echo "Get variable list"
