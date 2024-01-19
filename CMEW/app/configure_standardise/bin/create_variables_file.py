26586597df1391b1b0bf586bfa53388a3cfe3355
Josh Rackham <josh.rackham@metoffice.gov.uk>
2024-01-19 15:03:10 +0000

    #60: Added docstrings and made the functions more modular

--- CMEW/app/configure_standardise/bin/create_variables_file.py
+++ CMEW/app/configure_standardise/bin/create_variables_file.py
@@ -6,24 +6,43 @@ import os
 from pathlib import Path
 
 
-def extract_variables():
-    configure_standardise_path = Path(__file__).parent.parent
-    with open(configure_standardise_path / "mock_data/variables.txt") as file:
-        variables = file.read()
+def parse_variables_from_recipe(recipe_path):
+    """Reads and parses ESMValTool recipe into a formatted string of variables
+
+    Parameters
+    ----------
+    recipe_path : str
+        Location of the ESMValTool recipe file in the installed workflow
+
+    Returns
+    -------
+    str
+        Formatted string of the variables to be read by CDDS
+    """
+    with open(recipe_path) as source_file:
+        variables = source_file.read()
     return variables
 
 
-def write_variables(variables: str):
-    SHARE_DIR = os.environ["CYLC_WORKFLOW_SHARE_DIR"]
-    with open(SHARE_DIR + "/etc/variables.txt", mode="w+") as file:
-        file.write(variables)
-    return None
+def write_variables(variables, target_path):
+    """Writes string of variables to a text file in the installed workflow
+
+    Parameters
+    ----------
+    variables : str
+        Formatted string of the variables to be read by CDDS
+
+    target_path : str
+        Location to write the variables file
+    """
+    with open(target_path, "w+") as target_file:
+        target_file.write(variables)
 
 
 def main():
-    variables = extract_variables()
-    write_variables(variables=variables)
-    return None
+    mock_path = Path(__file__).parent.parent / "mock_data" / "variables.txt"
+    variables = parse_variables_from_recipe(mock_path)
+    write_variables(variables, os.environ["VARIABLES_PATH"])
 
 
 if __name__ == "__main__":
