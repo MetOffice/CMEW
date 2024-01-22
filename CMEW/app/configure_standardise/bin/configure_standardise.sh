1bbafd788e9bf9ae4ea94e83e5998f7d671c9723
dasha-shchep <28752245+dasha-shchep@users.noreply.github.com>
2024-01-22 11:06:08 +0000

    #112: Changed references to request.json and variables.txt to share/etc dir

--- CMEW/app/configure_standardise/bin/configure_standardise.sh
+++ CMEW/app/configure_standardise/bin/configure_standardise.sh
@@ -10,18 +10,12 @@ echo "Get variable list"
 # Running setup commands for CDDS
 
 export WORKING_DIRECTORY="${CYLC_WORKFLOW_SHARE_DIR}/data/cdds"
-
-mkdir -p ${WORKING_DIRECTORY}
-
 export ROOT_PROC_DIR="${WORKING_DIRECTORY}/proc"
 export ROOT_DATA_DIR="${WORKING_DIRECTORY}/cdds_data"
 
 mkdir -p ${ROOT_PROC_DIR}
 mkdir -p ${ROOT_DATA_DIR}
 
-mv ${CYLC_WORKFLOW_RUN_DIR}/app/configure_standardise/mock_data/request.json ${WORKING_DIRECTORY}/.
-mv ${CYLC_WORKFLOW_RUN_DIR}/app/configure_standardise/mock_data/variables.txt ${WORKING_DIRECTORY}/.
+cmew-standardise-env create_cdds_directory_structure "${CYLC_WORKFLOW_SHARE_DIR}/etc/request.json" -c "${ROOT_PROC_DIR}" -t "${ROOT_DATA_DIR}"
 
-cmew-standardise-env create_cdds_directory_structure "${WORKING_DIRECTORY}"/request.json -c "${ROOT_PROC_DIR}" -t "${ROOT_DATA_DIR}"
-
-cmew-standardise-env prepare_generate_variable_list "${WORKING_DIRECTORY}/request.json" -c "${ROOT_PROC_DIR}" -t "${ROOT_DATA_DIR}" --use_proc_dir -r "${WORKING_DIRECTORY}/variables.txt"
+cmew-standardise-env prepare_generate_variable_list "${CYLC_WORKFLOW_SHARE_DIR}/etc/request.json" -c "${ROOT_PROC_DIR}" -t "${ROOT_DATA_DIR}" --use_proc_dir -r "${CYLC_WORKFLOW_SHARE_DIR}/etc/variables.txt"
