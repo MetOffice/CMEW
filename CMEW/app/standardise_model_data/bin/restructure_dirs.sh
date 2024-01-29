06cd80e10b88f5380dde5f94653f38e703557fc7
nabilbrice <mo-nabilbrice@users.noreply.github.com>
2024-01-29 10:39:54 +0000

    154: added dir restructuring through ceda-mip-tools

---
+++ CMEW/app/standardise_model_data/bin/restructure_dirs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Send the output from 'set -x' to 'stdout' rather than 'stderr'.
+BASH_XTRACEFD=1
+set -eux
+
+CEDA_ROOT_DIR="/home/h03/hadmm/CDDS/github/ceda-mip-tools"
+RESTRUCTURE_COMMAND="/bin/restructure_for_cmip6"
+
+CDDS_DATA_DIR="${CYLC_WORKFLOW_SHARE_DIR}/data/cdds/cdds_data/"
+RESTRUCTURED_DIR="${CYLC_WORKFLOW_SHARE_DIR}/work/"
+
+echo "Restructuring directories from ${CDDS_DATA_DIR} to ${RESTRUCTURED_DIR}"
+cmew-standardise-env "${CEDA_ROOT_DIR}${RESTRUCTURE_COMMAND}" -d ${RESTRUCTURED_DIR} ${CDDS_DATA_DIR}
