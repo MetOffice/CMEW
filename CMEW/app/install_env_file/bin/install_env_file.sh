a2737a2a27615a6104fa40305ce1c60bdc2af956
Chris Billows <152496175+chrisbillowsMO@users.noreply.github.com>
2024-01-30 13:04:38 +0000

    #136: Define ENV_SCRIPTS_TARGET_DIR env variable

--- CMEW/app/install_env_file/bin/install_env_file.sh
+++ CMEW/app/install_env_file/bin/install_env_file.sh
@@ -6,11 +6,9 @@ set -eux
 # Paths for site-specific environment scripts.
 SOURCE_PROCESS_PATH="${CYLC_WORKFLOW_RUN_DIR}/site/${SITE}-process-env"
 SOURCE_STANDARDISE_PATH="${CYLC_WORKFLOW_RUN_DIR}/site/${SITE}-standardise-env"
-# Target directory for installation of the environment scripts.
-TARGET_DIR="${CYLC_WORKFLOW_SHARE_DIR}/bin"
 # Names of the environment scripts.
 ENV_PROCESS_FILE="cmew-process-env"
 ENV_STANDARDISE_FILE="cmew-standardise-env"
 # Copy the environment scripts to the 'bin' directory.
-cp "${SOURCE_PROCESS_PATH}" "${TARGET_DIR}/${ENV_PROCESS_FILE}"
-cp "${SOURCE_STANDARDISE_PATH}" "${TARGET_DIR}/${ENV_STANDARDISE_FILE}"
+cp "${SOURCE_PROCESS_PATH}" "${ENV_SCRIPTS_TARGET_DIR}/${ENV_PROCESS_FILE}"
+cp "${SOURCE_STANDARDISE_PATH}" "${ENV_SCRIPTS_TARGET_DIR}/${ENV_STANDARDISE_FILE}"
