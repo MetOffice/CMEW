c4c0d4ad06bad9ac294749452192a875aad5e9df
nabilbrice <mo-nabilbrice@users.noreply.github.com>
2024-01-31 16:23:44 +0000

    #154: moved cmew-standardise-env out of bash script into rose-app.conf

--- CMEW/app/standardise_model_data/bin/restructure_dirs.sh
+++ CMEW/app/standardise_model_data/bin/restructure_dirs.sh
@@ -9,7 +9,7 @@ RESTRUCTURE_COMMAND="/bin/restructure_for_cmip6"
 CDDS_DATA_DIR="${CYLC_WORKFLOW_SHARE_DIR}/data/cdds/cdds_data/"
 RESTRUCTURED_DIR="${CYLC_WORKFLOW_SHARE_DIR}/work/"
 
-cmew-standardise-env "${CEDA_ROOT_DIR}${RESTRUCTURE_COMMAND}" -d ${RESTRUCTURED_DIR} ${CDDS_DATA_DIR}
+${CEDA_ROOT_DIR}${RESTRUCTURE_COMMAND} -d ${RESTRUCTURED_DIR} ${CDDS_DATA_DIR}
 
 # ESMValTool reads from ./GCModelDev/CMIP/ so need to rename the directory.
 mv "${RESTRUCTURED_DIR}/GCModelDev/ESMVal/" "${RESTRUCTURED_DIR}/GCModelDev/CMIP/"
