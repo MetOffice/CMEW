89f7eb805c70da3d73cb44c4d9c06c3fd358b345
nabilbrice <mo-nabilbrice@users.noreply.github.com>
2024-01-31 16:52:23 +0000

    #154: tidied environment variables used in script

--- CMEW/app/standardise_model_data/bin/restructure_dirs.sh
+++ CMEW/app/standardise_model_data/bin/restructure_dirs.sh
@@ -6,10 +6,9 @@ set -eux
 CEDA_ROOT_DIR="/home/h03/hadmm/CDDS/github/ceda-mip-tools"
 RESTRUCTURE_COMMAND="/bin/restructure_for_cmip6"
 
-CDDS_DATA_DIR="${CYLC_WORKFLOW_SHARE_DIR}/data/cdds/cdds_data/"
-RESTRUCTURED_DIR="${CYLC_WORKFLOW_SHARE_DIR}/work/"
+ROOT_RESTRUCTURED_DIR="${CYLC_WORKFLOW_SHARE_DIR}/work/"
 
-${CEDA_ROOT_DIR}${RESTRUCTURE_COMMAND} -d ${RESTRUCTURED_DIR} ${CDDS_DATA_DIR}
+${CEDA_ROOT_DIR}${RESTRUCTURE_COMMAND} -d ${ROOT_RESTRUCTURED_DIR} ${ROOT_DATA_DIR}
 
 # ESMValTool reads from ./GCModelDev/CMIP/ so need to rename the directory.
 mv "${RESTRUCTURED_DIR}/GCModelDev/ESMVal/" "${RESTRUCTURED_DIR}/GCModelDev/CMIP/"
