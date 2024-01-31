ad909c01c1717eb5d53d56ee2185b447d99a652f
Josh Rackham <josh.rackham@metoffice.gov.uk>
2024-01-31 16:17:00 +0000

    #109: create_request now creates request from environment variables

--- CMEW/app/configure_standardise/bin/create_request_file.py
+++ CMEW/app/configure_standardise/bin/create_request_file.py
@@ -1,54 +1,77 @@
 #!/usr/bin/env python
+# (C) British Crown Copyright 2024, Met Office.
+# Please see LICENSE for license details.
 """
 Generates the request.json file from the ESMValTool recipe.
 """
 import os
 from pathlib import Path
+import json
 
 
-def parse_request_from_recipe(recipe_path):
-    """Retrieve request information from ESMValTool recipe.
-
-    Parameters
-    ----------
-    recipe_path : Path
-        Location of the ESMValTool recipe file in the installed workflow.
+def create_request():
+    """Retrieve CDDS request information from Rose suite configuration.
 
     Returns
     -------
-    str
-        Json-formatted string containing the request information.
+    dict
+        CDDS request information to be written to JSON file.
     """
-    with open(recipe_path) as file:
-        request = file.read()
+    stream = os.environ["STREAM"]
+    run_bounds_for_stream_key = f"run_bounds_for_stream{stream}"
+    run_bounds = f"{os.environ['START_DATETIME']} {os.environ['END_DATETIME']}"
+    request = {
+        "atmos_timestep": os.environ["ATMOS_TIMESTEP"],
+        "branch_method": "no parent",
+        "calendar": os.environ["CALENDAR"],
+        "child_base_date": "1850-01-01T00:00:00",
+        "config_version": "1.0.1",
+        "experiment_id": os.environ["EXPERIMENT_ID"],
+        "external_plugin": "",
+        "external_plugin_location": "",
+        "global_attributes": {},
+        "institution_id": os.environ["INSTITUTION_ID"],
+        "license": "GCModelDev model data is licensed under the Open Government License v3 (https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/)",  # noqa: E501
+        "mass_data_class": os.environ["MASS_DATA_CLASS"],
+        "mip": "ESMVal",
+        "mip_era": "GCModelDev",
+        "mip_table_dir": "/home/h03/cdds/etc/mip_tables/GCModelDev/0.0.9",
+        "model_id": os.environ["MODEL_ID"],
+        "model_type": os.environ["MODEL_TYPE"],
+        "package": "round-1",
+        "request_id": "CMEW",
+        "run_bounds": run_bounds,
+        run_bounds_for_stream_key: run_bounds,
+        "sub_experiment_id": "none",
+        "suite_branch": "trunk",
+        "suite_id": os.environ["SUITE_ID"],
+        "suite_revision": "not used except with data request",
+        "variant_label": os.environ["VARIANT_LABEL"],
+    }
     return request
 
 
 def write_request(request, target_path):
-    """Write json-formatted string to a json file in the installed workflow.
+    """Write request dict to a JSON file in the installed workflow.
 
     Parameters
     ----------
-    request : str
-        Json-formatted string containing the request information.
+    request : dict
+        Dictionary containing the request information.
 
     target_path: Path
         Location to write the request file.
     """
-    with open(target_path, mode="w+") as file:
-        file.write(request)
+    with open(target_path, mode="w") as file:
+        json.dump(request, file)
 
 
 def main():
-    # TODO: get the recipe_path from the environment
-    mock_request_path = (
-        Path(__file__).parent.parent / "mock_data" / "request.json"
-    )
     target_path = (
         Path(os.environ["CYLC_WORKFLOW_SHARE_DIR"]) / "etc" / "request.json"
     )
-    request = parse_request_from_recipe(mock_request_path)
-    write_request(request=request, target_path=target_path)
+    request = create_request()
+    write_request(request, target_path)
 
 
 if __name__ == "__main__":
