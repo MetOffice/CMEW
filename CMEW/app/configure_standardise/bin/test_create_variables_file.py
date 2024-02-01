bd51d912548093245ffbd7375b8de7189cdae42f
dasha-shchep <28752245+dasha-shchep@users.noreply.github.com>
2024-02-01 16:06:24 +0000

    #166: fix unit test parsing the variables.txt file

--- CMEW/app/configure_standardise/bin/test_create_variables_file.py
+++ CMEW/app/configure_standardise/bin/test_create_variables_file.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+# (C) British Crown Copyright 2024, Met Office.
+# Please see LICENSE for license details.
 """
 Tests for configure_standardise
 """
@@ -9,16 +11,16 @@ from pathlib import Path
 def test_parse_radiation_budget_variables():
     mock_path = Path(__file__).parent.parent / "mock_data" / "variables.txt"
     actual = parse_variables_from_recipe(mock_path)
-    expected = """Emon/rss
-Amon/rsdt
-Amon/rsut
-Amon/rsutcs
-Amon/rsds
-Emon/rls
-Amon/rlut
-Amon/rlutcs
-Amon/rlds
-Amon/hfss
-Amon/hfls
+    expected = """Emon/rss:apm
+Amon/rsdt:apm
+Amon/rsut:apm
+Amon/rsutcs:apm
+Amon/rsds:apm
+Emon/rls:apm
+Amon/rlut:apm
+Amon/rlutcs:apm
+Amon/rlds:apm
+Amon/hfss:apm
+Amon/hfls:apm
 """
     assert actual == expected
