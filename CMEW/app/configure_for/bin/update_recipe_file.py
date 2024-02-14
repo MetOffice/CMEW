f3fcc0c085808b573cf26e5f2631dea88d13a7be
Chris Billows <152496175+chrisbillowsMO@users.noreply.github.com>
2024-02-14 09:29:54 +0000

    #18: Create skeleton functions and red test.

---
+++ CMEW/app/configure_for/bin/update_recipe_file.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# (C) British Crown Copyright 2024, Met Office.
+# Please see LICENSE for license details.
+import os
+from pathlib import Path
+import yaml
+
+def update_recipe(recipe_path):
+    with open(recipe_path, 'r') as f:
+        original_recipe = yaml.safe_load(f)
+    updated_recipe = original_recipe
+    return updated_recipe
+
+def write_recipe(recipe, target_path):
+    with open(target_path, 'w') as f:
+        yaml.dump(recipe, f)
+
+def main():
+    recipe_path = os.environ["RECIPE_NAME"]
+    updated_recipe = update_recipe(recipe_path)
+    write_recipe(updated_recipe, recipe_path)
+
+if __name__ == "__main__":
+    main()
+       
+    # FOR QUICK PYYAML TESTS
+    # with open('/net/home/h02/cbillows/Code/CMEW/CMEW/app/configure_for/recipe_data/recipe_radiation_budget.yml', 'r') as file:
+    #     data = yaml.safe_load(file)
+    # print(data)
