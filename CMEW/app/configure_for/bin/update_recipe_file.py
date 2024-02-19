6e9cdb95a28eb315ed7e8336e50727426e7bc74d
Chris Billows <152496175+chrisbillowsMO@users.noreply.github.com>
2024-02-19 10:55:31 +0000

    #18: Add docstrings.

--- CMEW/app/configure_for/bin/update_recipe_file.py
+++ CMEW/app/configure_for/bin/update_recipe_file.py
@@ -1,11 +1,57 @@
 #!/usr/bin/env python
 # (C) British Crown Copyright 2024, Met Office.
 # Please see LICENSE for license details.
+"""
+Overwrite the ESMValTool recipe with an updated version. Include:
+
+* CMEW required values
+* User configurable variables from the Rose suite configuration
+"""
 import os
 import yaml
 
 
 def update_recipe(recipe_path):
+    """Update the ESMValTool recipe.
+
+    * Read the ESMValTool recipe YAML file from the provided ``recipe_path``
+    * Update the dataset section of the recipe with a) CMEW required key/values
+      and b) user configurables values from the Rose suite configuration.
+
+    Recipe file/datasets section snippet (human written YAML)::
+
+    datasets:
+      - {dataset: <dataset>, project: <project>, exp: <exp>,
+         ensemble: <ensemble>, grid: <grid>, start_year: <start_year>,
+         end_year: <end_year>}
+      - {dataset: <dataset>, project: <project>, exp: <exp>,
+         ensemble: <ensemble>, grid: <grid>, start_year: <start_year>,
+         end_year: <end_year>}
+
+    Updated recipe file/datasets section snippet (machine written YAML)::
+
+    datasets:
+    - {dataset: <dataset>, end_year: <end_year>, ensemble: <ensemble>,
+      end_year: <end_year>, exp: <exp>, grid: <grid>, project: <project>,
+      start_year: <start_year>}
+    - {activity: <activity>, dataset: <dataset>, end_year: <end_year>,
+      ensemble: <ensemble>, exp: <exp>, grid: <grid>, project: <project>,
+      start_year: <start_year>}
+
+    Notes
+    -----
+    The updated recipe includes one additional CMEW required key: "Activity".
+
+    Parameters
+    ----------
+    recipe_path: str
+        Location of the ESMValTool recipe file.
+
+    Returns
+    -------
+    recipe: dict[str, union[str, int]]
+        The content of the ESMValTool recipe with updated datasets section.
+    """
     start_year = int(os.environ["START_YEAR"])
     end_year = (
         int(os.environ["START_YEAR"]) + int(os.environ["NUMBER_OF_YEARS"]) - 1
@@ -29,11 +75,25 @@ def update_recipe(recipe_path):
 
 
 def write_recipe(updated_recipe, target_path):
+    """Write updated ESMValTool recipe to a YAML file at ``target_path``.
+
+    Parameters
+    ----------
+    updated_recipe: dict
+        Dictionary containing the updated ESMValTool recipe content.
+
+    target_path: str
+        Location to write the updated ESMValTool recipe.
+    """
     with open(target_path, "w") as f:
         yaml.dump(updated_recipe, f)
 
 
 def main():
+    """
+    Load and update the ESMValTool recipe. Overwrite the original recipe with
+    the updated recipe.
+    """
     recipe_path = os.environ["RECIPE_PATH"]
     updated_recipe = update_recipe(recipe_path)
     write_recipe(updated_recipe, recipe_path)
