bb7b563a27ec6bafeadc1d606cc7acc6638e12ec
Ed Gillett <146008263+mo-gill@users.noreply.github.com>
2024-02-26 15:45:21 +0000

    #230: remove print statement

--- CMEW/app/configure_process/bin/test_configure_process.py
+++ CMEW/app/configure_process/bin/test_configure_process.py
@@ -6,7 +6,7 @@ from configure_process import create_user_config_file
 
 def test_create_user_config_file_verify_remove_preproc_dir_value_is_false():
     config_values = create_user_config_file()
-    print(config_values)
+
     output = config_values["remove_preproc_dir"]
     expected = False
     assert output == expected
