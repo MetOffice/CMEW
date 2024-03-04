aaaf81ba4a870a569cac432dd8abb52a3d975aa4
Ed Gillett <146008263+mo-gill@users.noreply.github.com>
2024-03-04 14:11:35 +0000

    #230: refactor test 5 to be parametrized

--- CMEW/app/configure_process/bin/test_configure_process.py
+++ CMEW/app/configure_process/bin/test_configure_process.py
@@ -2,6 +2,7 @@
 # (C) Crown Copyright 2024, Met Office.
 # Please see LICENSE.md for license details.
 from configure_process import create_user_config_file
+import pytest
 
 
 def test_create_user_config_file_verify_remove_preproc_dir_value_is_false():
@@ -35,4 +36,29 @@ def test_create_user_config_file_verify_cmip6_rootpath():
     config_values = create_user_config_file(test_values)
 
     output = config_values["rootpath"]["CMIP6"]
+    assert output == expected
+
+
+# test 5
+# def test_create_user_config_file_two_values():
+#     expected = "obspath"
+#     test_values = {"DRS_OBS": expected}
+#     config_values = create_user_config_file(test_values)
+
+#     output = config_values["drs"]["OBS"]
+#     assert output == expected
+
+# test 5 parametrized
+
+
+@pytest.mark.parametrize(
+    "outer_key, input_key, key_assigned, expected",
+    [("drs", "DRS_OBS", "OBS", "obspath")],
+)
+def test_create_user_config_file_two_values(
+    outer_key, input_key, key_assigned, expected
+):
+    test_values = {input_key: expected}
+    config_values = create_user_config_file(test_values)
+    output = config_values[outer_key][key_assigned]
     assert output == expected
