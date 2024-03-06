1da04405c3c445460251b5c11c5cdf4875c16073
Ed Gillett <146008263+mo-gill@users.noreply.github.com>
2024-03-06 08:58:14 +0000

    #230: refactoring remaining unit tests to parameterised

--- CMEW/app/configure_process/bin/test_configure_process.py
+++ CMEW/app/configure_process/bin/test_configure_process.py
@@ -5,37 +5,17 @@ import pytest
 from configure_process import create_user_config_file
 
 
-def test_create_user_config_file_verify_remove_preproc_dir_value_is_false():
-    config_values = create_user_config_file()
-
-    output = config_values["remove_preproc_dir"]
-    expected = False
-    assert output == expected
-
-
-def test_create_user_config_file_verify_esmval_value_equals_BADC():
-    config_values = create_user_config_file()
-
-    output = config_values["drs"]["ESMVal"]
-    expected = "BADC"
-    assert output == expected
-
-
-def test_create_user_config_file_verify_user_config_path():
-    expected = "userpath"
-    test_values = {"USER_CONFIG_PATH": expected}
+@pytest.mark.parametrize(
+    "input_key, output_key, expected",
+    [
+        ("remove_preproc_dir", "remove_preproc_dir", False),
+        ("USER_CONFIG_PATH", "config_file", "userpath"),
+    ],
+)
+def test_create_user_config_file_two_values(input_key, output_key, expected):
+    test_values = {input_key: expected}
     config_values = create_user_config_file(test_values)
-
-    output = config_values["config_file"]
-    assert output == expected
-
-
-def test_create_user_config_file_verify_cmip6_rootpath():
-    expected = "cmip_rootpath"
-    test_values = {"ROOTPATH_CMIP6": expected}
-    config_values = create_user_config_file(test_values)
-
-    output = config_values["rootpath"]["CMIP6"]
+    output = config_values[output_key]
     assert output == expected
 
 
@@ -44,9 +24,11 @@ def test_create_user_config_file_verify_cmip6_rootpath():
     [
         ("DRS_CORDEX", "drs", "CORDEX", "cordexpath"),
         ("DRS_OBS", "drs", "OBS", "obspath"),
+        ("ROOTPATH_CMIP6", "rootpath", "CMIP6", "cmip_rootpath"),
+        ("ESMVal", "drs", "ESMVal", "BADC"),
     ],
 )
-def test_create_user_config_file_two_values(
+def test_create_user_config_file_four_values(
     input_key, output_outer_key, output_inner_key, expected
 ):
     test_values = {input_key: expected}
