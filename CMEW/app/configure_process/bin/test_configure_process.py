32d11410d7ccc41be2ab7ea706bea0b4ad9cc645
Ed <146008263+mo-gill@users.noreply.github.com>
2024-03-08 12:21:21 +0000

    234: Add unit tests for the `create_user_config_file` function
    * #230: attempt to fix environment variables issue
    
    * #230: addition of conditionals for two dictionary items to remedy testing module errors
    
    * #230: pytest for configure_process.py
    
    * #230: correction of copyright
    
    * #230: Alter test name to fit working practices
    
    * #230: remove print statement
    
    * #230: add pytest function to check a specific value in configure_process is BADC
    
    * #230: refactor of two first tests into one parametrized test
    
    * #230: changing parametrized back to two unit tests
    
    * #230: Third test case added
    
    * #230: Fourth test case added to verify cmip rootpath
    
    * 230: refactor test 5 to be parametrized
    
    * #230: refactor test 5 to be parametrized
    
    * #230: add second value check to parametrized test
    
    * #230: commented mock test removed
    
    * #230: change of parameter names
    
    * #230: refactoring remaining unit tests to parameterised
    
    * #230: Name change on both tests
    
    * #230: Relocate test into new tests folder in app/unittest
    
    * #230: Relocate test into new tests folder in app/unittest
    
    * #230: moved test file back into bin folder and removed unittest folder. Also corrected first parametrized test
    
    * #230: moved test file back into bin folder and removed unittest folder. Also corrected first parametrized test
    
    * #230: Correction of hardcoded tests by adding conditionals
    
    * #230: Fix file paths passed as tuples.
    
    * #230: Rename output as actual in test assert.
    
    ---------
    
    Co-authored-by: chrisbillowsMO <152496175+chrisbillowsMO@users.noreply.github.com>

---
+++ CMEW/app/configure_process/bin/test_configure_process.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# (C) Crown Copyright 2024, Met Office.
+# Please see LICENSE.md for license details.
+import pytest
+from configure_process import create_user_config_file
+
+
+@pytest.mark.parametrize(
+    "input_key, output_key, expected",
+    [
+        (None, "remove_preproc_dir", False),
+        ("USER_CONFIG_PATH", "config_file", "userpath"),
+    ],
+)
+def test_create_user_config_file_single_values(
+    input_key, output_key, expected
+):
+    if input_key is None:
+        test_values = None
+    else:
+        test_values = {input_key: expected}
+
+    config_values = create_user_config_file(test_values)
+    actual = config_values[output_key]
+    assert actual == expected
+
+
+@pytest.mark.parametrize(
+    "input_key, output_outer_key, output_inner_key, expected",
+    [
+        ("DRS_CORDEX", "drs", "CORDEX", "cordexpath"),
+        ("DRS_OBS", "drs", "OBS", "obspath"),
+        ("ROOTPATH_CMIP6", "rootpath", "CMIP6", "cmip_rootpath"),
+        ("ESMVal", "drs", "ESMVal", "BADC"),
+    ],
+)
+def test_create_user_config_file_nested_values(
+    input_key, output_outer_key, output_inner_key, expected
+):
+    if input_key is None:
+        test_values = None
+    else:
+        test_values = {input_key: expected}
+
+    config_values = create_user_config_file(test_values)
+    actual = config_values[output_outer_key][output_inner_key]
+    assert actual == expected
