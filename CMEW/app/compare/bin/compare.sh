15c8fc7d32eadc2c4cba027933364ae467eb94e0
mo-johnmo <193518103+mo-johnmo@users.noreply.github.com>
2025-03-14 08:42:25 +0000

    #255 Fix compare.sh problem and annotate.

--- CMEW/app/compare/bin/compare.sh
+++ CMEW/app/compare/bin/compare.sh
@@ -20,8 +20,12 @@ set -eux
 for metric_file in ${METRIC_FILES}
     do
         OUTPUT_METRIC_PATH="${OUTPUT_DIR}/${METRIC_DIR}*/${metric_file}"
-        if [ ! -f "${OUTPUT_METRIC_PATH}" ]; then
+#       Need globbing but not word splitting. IFS is internal field separator
+#        IFS=""
+#       shellcheck disable=SC2086
+        if [ ! -f ${OUTPUT_METRIC_PATH} ]; then
             echo "${OUTPUT_METRIC_PATH} does not exist"
             exit 1
         fi
+        unset IFS
     done
