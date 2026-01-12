bd7aef2cdb9b220c35b816b1dcc44193df025498
zmaalick <zubair.maalick@metoffice.gov.uk>
2026-01-12 09:54:53 +0000

    rename the cdds runs with custom name #164

--- CMEW/app/configure_standardise/bin/create_request_file.py
+++ CMEW/app/configure_standardise/bin/create_request_file.py
@@ -45,7 +45,7 @@ def create_request():
         "package": "round-1",
         "root_proc_dir": os.environ["ROOT_PROC_DIR"],
         "root_data_dir": os.environ["ROOT_DATA_DIR"],
-        "workflow_basename": "CMEW",
+        "workflow_basename": f"{os.environ['MODEL_ID']}_historical_{os.environ['VARIANT_LABEL']}_apm",
     }
     request["data"] = {
         "end_date": f"{end_year}-01-01T00:00:00",
