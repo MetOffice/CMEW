c15ab85bc50ba20ef39b187f10a0b01c542ce991
Naomi Parsons <naomi.parsons@metoffice.gov.uk>
2026-02-06 16:35:50 +0000

    Renaming app and adding .py file with executable permissions

---
+++ CMEW/app/add_datasets/bin/copy_datasets_to_share.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+# TODO: include this functionality here then remove from rose-app.conf
+# [file:$DATASETS_LIST_DIR]
+# mode=mkdir
+
+# TODO: take processing nl format out of configure for
+# TODO: think about where model datasets end up
+
+# TODO: remove this line from rose-app.conf once not needed
+# =cp cmip6_datasets.nl $DATASETS_LIST_DIR /
+
+if __name__ == "__main__":
+    pass
