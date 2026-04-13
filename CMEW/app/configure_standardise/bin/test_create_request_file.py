0143ba48dfefb836c501d5521386936f6d0c6f6c
alistair.sellar <16133375+alistairsellar@users.noreply.github.com>
2026-04-13 14:25:15 +0100

    Move expected result back into main test function
    to simplify conflicts with main

--- CMEW/app/configure_standardise/bin/test_create_request_file.py
+++ CMEW/app/configure_standardise/bin/test_create_request_file.py
@@ -6,8 +6,33 @@ import os
 from create_request_file import create_request, load_request_defaults
 
 
-def expected_request():
-    """Return the expected request."""
+def test_create_request(monkeypatch, tmp_path):
+    # Note that this test only works within the workflow due to the
+    # following environment variable use. For command line pytest use, export
+    # CYLC_WORKFLOW_RUN_DIR to point to a previous workflow run dir, or
+    # workflow source dir.
+    request_defaults_path = os.path.join(
+        os.environ.get("CYLC_WORKFLOW_RUN_DIR"),
+        "app/configure_standardise/etc/request_defaults.cfg",
+    )
+
+    monkeypatch.setenv("START_YEAR", "1993")
+    monkeypatch.setenv("NUMBER_OF_YEARS", "1")
+    monkeypatch.setenv("CALENDAR", "360_day")
+    monkeypatch.setenv("INSTITUTION_ID", "MOHC")
+    monkeypatch.setenv("MODEL_ID", "UKESM1-0-LL")
+    monkeypatch.setenv("ROOT_PROC_DIR", "/path/to/proc/dir/")
+    monkeypatch.setenv("ROOT_DATA_DIR", "/path/to/data/dir/")
+    monkeypatch.setenv("SUITE_ID", "u-az513")
+    monkeypatch.setenv("VARIABLES_PATH", "/path/to/variables.txt")
+    monkeypatch.setenv("VARIANT_LABEL", "r1i1p1f1")
+    monkeypatch.setenv("REQUEST_DEFAULTS_PATH", str(request_defaults_path))
+
+    config = create_request()
+    actual = {
+        section: dict(config.items(section)) for section in config.sections()
+    }
+
     request_defaults = load_request_defaults()
     expected = {}
     for section in request_defaults.sections():
@@ -39,36 +64,5 @@ def expected_request():
             "variable_list_file": "/path/to/variables.txt",
         }
     )
-
-    return expected
-
-
-def test_create_request(monkeypatch, tmp_path):
-    # Note that this test only works within the workflow due to the
-    # following environment variable use. For command line pytest use, export
-    # CYLC_WORKFLOW_RUN_DIR to point to a previous workflow run dir, or
-    # workflow source dir.
-    request_defaults_path = os.path.join(
-        os.environ.get("CYLC_WORKFLOW_RUN_DIR"),
-        "app/configure_standardise/etc/request_defaults.cfg",
-    )
-
-    monkeypatch.setenv("START_YEAR", "1993")
-    monkeypatch.setenv("NUMBER_OF_YEARS", "1")
-    monkeypatch.setenv("CALENDAR", "360_day")
-    monkeypatch.setenv("INSTITUTION_ID", "MOHC")
-    monkeypatch.setenv("MODEL_ID", "UKESM1-0-LL")
-    monkeypatch.setenv("ROOT_PROC_DIR", "/path/to/proc/dir/")
-    monkeypatch.setenv("ROOT_DATA_DIR", "/path/to/data/dir/")
-    monkeypatch.setenv("SUITE_ID", "u-az513")
-    monkeypatch.setenv("VARIABLES_PATH", "/path/to/variables.txt")
-    monkeypatch.setenv("VARIANT_LABEL", "r1i1p1f1")
-    monkeypatch.setenv("REQUEST_DEFAULTS_PATH", str(request_defaults_path))
-
-    config = create_request()
-    actual = {
-        section: dict(config.items(section)) for section in config.sections()
-    }
-    expected = expected_request()
 
     assert actual == expected
