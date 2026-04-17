116bdc08c70595dbd8b2e759455e1b26fe532de4
alistair.sellar <16133375+alistairsellar@users.noreply.github.com>
2026-04-17 10:00:13 +0100

    Move cdds_convert from script back into rose-app.conf
    And remove script

--- CMEW/app/standardise_model_data/bin/standardise_model_data.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# (C) Crown Copyright 2025, Met Office.
-# The LICENSE.md file contains full licensing details.
-# Standardise data for both REF and EVAL runs via CDDS.
-
-BASH_XTRACEFD=1
-set -eux
-
-cmew-standardise-env cdds_convert "${REQUEST_PATH}"
