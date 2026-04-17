4435d335e96bad85a7fef565d3d134fd03113b0e
alistair.sellar <16133375+alistairsellar@users.noreply.github.com>
2026-04-17 11:13:07 +0100

    Introduce python vars to avoid duplication of values

--- CMEW/app/configure_standardise/bin/test_create_request_file.py
+++ CMEW/app/configure_standardise/bin/test_create_request_file.py
@@ -11,24 +11,34 @@ def test_create_request(monkeypatch):
     request_defaults_path = (
         Path(__file__).parent.parent / "etc" / "request_defaults.cfg"
     )
-    monkeypatch.setenv("REQUEST_DEFAULTS_PATH", str(request_defaults_path))
+    start_year = 1993
+    number_of_years = 1
+    calendar = "standard"
+    experiment_id = "amip"
+    institution_id = "MOHC"
+    model_id = "HadGEM3-GC31-HH"
+    root_proc_dir = "/path/to/proc/dir/"
+    root_data_dir = "/path/to/data/dir/"
+    suite_id = "u-az513"
+    variables_path = "/path/to/variables.txt"
+    variant_label = "r1i1p1f1"
+    mip_table_dir = "~cdds/etc/mip_tables/GCModelDev/0.0.25"
+    stream_id = "apm"
 
-    monkeypatch.setenv("START_YEAR", "1993")
-    monkeypatch.setenv("NUMBER_OF_YEARS", "1")
-    monkeypatch.setenv("CALENDAR", "standard")
-    monkeypatch.setenv("EXPERIMENT_ID", "amip")
-    monkeypatch.setenv("INSTITUTION_ID", "MOHC")
-    monkeypatch.setenv("MODEL_ID", "HadGEM2-GC31-HH")
-    monkeypatch.setenv("ROOT_PROC_DIR", "/path/to/proc/dir/")
-    monkeypatch.setenv("ROOT_DATA_DIR", "/path/to/data/dir/")
-    monkeypatch.setenv("SUITE_ID", "u-az513")
-    monkeypatch.setenv("VARIABLES_PATH", "/path/to/variables.txt")
-    monkeypatch.setenv("VARIANT_LABEL", "r1i1p1f1")
-    monkeypatch.setenv(
-        "MIP_TABLE_DIR",
-        "~cdds/etc/mip_tables/GCModelDev/0.0.25",
-    )
-    monkeypatch.setenv("STREAM_ID", "apm")
+    monkeypatch.setenv("REQUEST_DEFAULTS_PATH", str(request_defaults_path))
+    monkeypatch.setenv("START_YEAR", str(start_year))
+    monkeypatch.setenv("NUMBER_OF_YEARS", str(number_of_years))
+    monkeypatch.setenv("CALENDAR", calendar)
+    monkeypatch.setenv("EXPERIMENT_ID", experiment_id)
+    monkeypatch.setenv("INSTITUTION_ID", institution_id)
+    monkeypatch.setenv("MODEL_ID", model_id)
+    monkeypatch.setenv("ROOT_PROC_DIR", root_proc_dir)
+    monkeypatch.setenv("ROOT_DATA_DIR", root_data_dir)
+    monkeypatch.setenv("SUITE_ID", suite_id)
+    monkeypatch.setenv("VARIABLES_PATH", variables_path)
+    monkeypatch.setenv("VARIANT_LABEL", variant_label)
+    monkeypatch.setenv("MIP_TABLE_DIR", mip_table_dir)
+    monkeypatch.setenv("STREAM_ID", stream_id)
 
     config = create_request()
     actual = {
@@ -42,29 +52,27 @@ def test_create_request(monkeypatch):
 
     expected["metadata"] = {
         **request_defaults["metadata"],
-        "calendar": "standard",
-        "experiment_id": "amip",
-        "institution_id": "MOHC",
-        "model_id": "HadGEM2-GC31-HH",
-        "sub_experiment_id": "uaz513",
-        "variant_label": "r1i1p1f1",
+        "calendar": calendar,
+        "experiment_id": experiment_id,
+        "institution_id": institution_id,
+        "model_id": model_id,
+        "sub_experiment_id": suite_id.replace("-", ""),
+        "variant_label": variant_label,
     }
     expected["common"] = {
         **request_defaults["common"],
-        "mip_table_dir": os.path.expanduser(
-            "~cdds/etc/mip_tables/GCModelDev/0.0.25",
-        ),
-        "root_proc_dir": "/path/to/proc/dir/",
-        "root_data_dir": "/path/to/data/dir/",
-        "workflow_basename": "u-az513",
+        "mip_table_dir": os.path.expanduser(mip_table_dir),
+        "root_proc_dir": root_proc_dir,
+        "root_data_dir": root_data_dir,
+        "workflow_basename": suite_id,
     }
     expected["data"] = {
         **request_defaults["data"],
-        "end_date": "1994-01-01T00:00:00",
-        "model_workflow_id": "u-az513",
-        "start_date": "1993-01-01T00:00:00",
-        "streams": "apm",
-        "variable_list_file": "/path/to/variables.txt",
+        "end_date": str(start_year + number_of_years) + "-01-01T00:00:00",
+        "model_workflow_id": suite_id,
+        "start_date": str(start_year) + "-01-01T00:00:00",
+        "streams": stream_id,
+        "variable_list_file": variables_path,
     }
 
     assert actual == expected
