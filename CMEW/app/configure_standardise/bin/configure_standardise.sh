64cef42401f658b735345b21a442552aa174402c
alistair.sellar <16133375+alistairsellar@users.noreply.github.com>
2026-04-17 09:58:11 +0100

    Return `configure_standardisse.sh` to previous version
    Swaps create_variables and create_request, relative to last commit

--- CMEW/app/configure_standardise/bin/configure_standardise.sh
+++ CMEW/app/configure_standardise/bin/configure_standardise.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-# (C) Crown Copyright 2024-2026, Met Office.
+# (C) Crown Copyright 2024-2025, Met Office.
 # The LICENSE.md file contains full licensing details.
 # Send the output from 'set -x' to 'stdout' rather than 'stderr'.
 BASH_XTRACEFD=1
 set -eux
 
-cmew-esmvaltool-env create_variables_file.py
+echo "Running configure_standardise"
+
+# Create request configuration file and variables file.
 cmew-esmvaltool-env create_request_file.py
+cmew-esmvaltool-env create_variables_file.py
+
+# Create CDDS directory structure and variables list.
 cmew-standardise-env create_cdds_directory_structure "${REQUEST_PATH}"
 cmew-standardise-env prepare_generate_variable_list "${REQUEST_PATH}"
