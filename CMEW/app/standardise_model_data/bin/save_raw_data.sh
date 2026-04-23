c3873b98955a739e662d2ab4bbfb751d1f96c7e6
Alistair Sellar <16133375+alistairsellar@users.noreply.github.com>
2026-04-23 08:50:27 +0100

    Missing punctuation
    Co-authored-by: Naomi Parsons <naomi.parsons@metoffice.gov.uk>

--- CMEW/app/standardise_model_data/bin/save_raw_data.sh
+++ CMEW/app/standardise_model_data/bin/save_raw_data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # (C) Crown Copyright 2026, Met Office.
 # The LICENSE.md file contains full licensing details.
-# Copy extracted raw model run data
+# Copy extracted raw model run data.
 
 BASH_XTRACEFD=1
 set -xeu
