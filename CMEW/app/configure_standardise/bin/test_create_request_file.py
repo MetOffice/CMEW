1c7c4174d3bd371ff1bf20ddb018e12e068719b6
Naomi Parsons <naomi.parsons@metoffice.gov.uk>
2026-04-27 15:12:35 +0100

    Using model_runs yaml instead of environment variables

--- CMEW/app/configure_standardise/bin/test_create_request_file.py
+++ CMEW/app/configure_standardise/bin/test_create_request_file.py
@@ -1,6 +1,12 @@
 # (C) Crown Copyright 2024-2026, Met Office.
 # The LICENSE.md file contains full licensing details.
+"""
+Unit tests for add_datasets_to_share.py
 
+Test data files:
+/app/unittest/mock_data/model_runs.yml
+    input for test_create_request
+"""
 import os
 from pathlib import Path
 
@@ -8,52 +14,25 @@ from create_request_file import create_request, load_request_defaults
 
 
 def test_create_request(monkeypatch):
-    # Two runs only: must set BOTH eval and ref environment variables
-    # and set CYLC_TASK_PARAM_dataset to select which request to generate.
+    monkeypatch.setenv("DATASETS_LIST_DIR", str(Path(__file__).parent.parent.parent / "unittest" / "mock_data" ))
 
     request_defaults_path = (
         Path(__file__).parent.parent / "etc" / "request_defaults.cfg"
     )
-    start_year = 1993
-    number_of_years = 1
-    calendar = "standard"
-    experiment_id = "amip"
-    institution_id = "MOHC"
-    model_id = "HadGEM3-GC31-HH"
     root_proc_dir = "/path/to/proc/dir/"
     root_data_dir = "/path/to/data/dir/"
-    suite_id = "u-az513"
     variables_path = "/path/to/variables.txt"
-    variant_label = "r1i1p1f1"
     mip_table_dir = "~cdds/etc/mip_tables/GCModelDev/0.0.25"
     stream_id = "apm"
 
     monkeypatch.setenv("REQUEST_DEFAULTS_PATH", str(request_defaults_path))
-    monkeypatch.setenv("START_YEAR", str(start_year))
-    monkeypatch.setenv("NUMBER_OF_YEARS", str(number_of_years))
-    monkeypatch.setenv("CALENDAR", calendar)
-    monkeypatch.setenv("EXPERIMENT_ID", experiment_id)
-    monkeypatch.setenv("INSTITUTION_ID", institution_id)
-    monkeypatch.setenv("MODEL_ID", model_id)
     monkeypatch.setenv("ROOT_PROC_DIR", root_proc_dir)
     monkeypatch.setenv("ROOT_DATA_DIR", root_data_dir)
-    monkeypatch.setenv("SUITE_ID", suite_id)
     monkeypatch.setenv("VARIABLES_PATH", variables_path)
-    monkeypatch.setenv("VARIANT_LABEL", variant_label)
     monkeypatch.setenv("MIP_TABLE_DIR", mip_table_dir)
     monkeypatch.setenv("STREAM_ID", stream_id)
 
-    # Reference run environment (values not used in this test)
-    monkeypatch.setenv("REF_CALENDAR", "unused")
-    monkeypatch.setenv("REF_MODEL_ID", "unused")
-    monkeypatch.setenv("REF_SUITE_ID", "unused")
-    monkeypatch.setenv("REF_EXPERIMENT_ID", "unused")
-    monkeypatch.setenv("REF_VARIANT_LABEL", "unused")
-
-    # Select evaluation run
-    monkeypatch.setenv("CYLC_TASK_PARAM_dataset", "u-az513")
-
-    config = create_request()
+    config = create_request("u-cw673")
     actual = {
         section: dict(config.items(section)) for section in config.sections()
     }
@@ -65,24 +44,24 @@ def test_create_request(monkeypatch):
 
     expected["metadata"] = {
         **request_defaults["metadata"],
-        "calendar": calendar,
-        "experiment_id": experiment_id,
-        "institution_id": institution_id,
-        "model_id": model_id,
-        "variant_label": variant_label,
+        "calendar": "gregorian",
+        "experiment_id": "amip",
+        "institution_id": "MOHC",
+        "model_id": "HadGEM3-GC5E-LL",
+        "variant_label": "r1i1p1f1",
     }
     expected["common"] = {
         **request_defaults["common"],
         "mip_table_dir": os.path.expanduser(mip_table_dir),
         "root_proc_dir": root_proc_dir,
         "root_data_dir": root_data_dir,
-        "workflow_basename": suite_id,
+        "workflow_basename": "u-cw673",
     }
     expected["data"] = {
         **request_defaults["data"],
-        "end_date": f"{str(start_year + number_of_years)}-01-01T00:00:00",
-        "model_workflow_id": suite_id,
-        "start_date": f"{str(start_year)}-01-01T00:00:00",
+        "end_date": "2002-01-01T00:00:00",
+        "model_workflow_id": "u-cw673",
+        "start_date": "1993-01-01T00:00:00",
         "streams": stream_id,
         "variable_list_file": variables_path,
     }
