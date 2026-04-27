3a22a4af5c1121255d21e3098bad22ce995d44c6
Naomi Parsons <naomi.parsons@metoffice.gov.uk>
2026-04-27 13:24:09 +0100

    Adding unit test
    Some of the content of this commit has been produced with the assistance of GitHub Copilot Enterprise

---
+++ CMEW/app/unittest/tests/test_scrape_ini.py
@@ -0,0 +1,32 @@
+# (C) Crown Copyright 2026, Met Office.
+# The LICENSE.md file contains full licensing details.
+"""Unit tests for CMEW/lib/python/scrape_ini.py"""
+from pathlib import Path
+import importlib.util
+import pytest
+
+# --- Section to import scrape_ini.py ---
+
+# PYTHONPATH doesn't autmatically pick this up
+scrape_ini_path = Path(__file__).parent.parent.parent.parent / "lib" / "python" / "scrape_ini.py"
+
+# The next three lines are with help from GiHub Copilot Enterprise
+spec = importlib.util.spec_from_file_location("scrape_ini", scrape_ini_path)
+scrape_ini = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(scrape_ini)
+
+# --- End of import section ---
+
+@pytest.fixture
+def path_to_mock_rose_suite():
+    path = (
+        Path(__file__).parent.parent
+        / "mock_data"
+        / "mock_rose_suite_conf.ini"
+    )
+    return str(path)
+
+def test_list_datasets(path_to_mock_rose_suite):
+    expected = ["u-cw673", "u-bv526"]
+    actual = scrape_ini.list_datasets(path_to_mock_rose_suite)
+    assert actual == expected
