4441c6bdd3a30e0d842bba11139d3ed440ceecc2
Naomi Parsons <naomi.parsons@metoffice.gov.uk>
2026-06-12 14:10:29 +0100

    #482: Add logging in to `CMEW/lib/python`
    Logging as print statements

--- CMEW/lib/python/scrape_ini.py
+++ CMEW/lib/python/scrape_ini.py
@@ -30,6 +30,7 @@ def extract_suite_ids(content):
 
         # Look for relevant lines
         if line.startswith("suite_id"):
+            print(f"[scrape_ini.py] Found line: {line}")
 
             # Take the value of the suite ID
             suite_id = line.split("=")[1]
@@ -37,6 +38,7 @@ def extract_suite_ids(content):
             # Add to the list, unquoted
             suite_ids.append(suite_id.strip().replace('"', ""))
 
+    print(f"[scrape_ini.py] Suite IDs found: {suite_ids}")
     return suite_ids
 
 
@@ -67,6 +69,7 @@ def list_datasets(fp):
     # Write the list as a comma separated string
     datasets_string = ", ".join(datasets)
 
+    print(f"[scrape_ini.py] Datasets found: {datasets_string}")
     return datasets_string
 
 
@@ -74,6 +77,9 @@ def retrieve_value(fp, indicator, key):
     """
     Return the value of a key from the indicated section of an ini-style file.
     """
+    # Provide a default in case things go wrong
+    value = None
+
     # Read the file
     with open(fp, "r") as f:
         content = f.readlines()
@@ -95,24 +101,33 @@ def retrieve_value(fp, indicator, key):
                     # Break the inner loop
                     break
 
+    print(f"[scrape_ini.py] Retrieved value: {value}")
     return value
 
 
 def find_ref(fp):
     """Return the reference suite ID"""
-    return retrieve_value(fp, "reference", "suite_id")
+    ref_suite_id = retrieve_value(fp, "reference", "suite_id")
+    print(f"[scrape_ini.py] Ref suite ID: {ref_suite_id}")
+    return ref_suite_id
 
 
 def find_eval(fp):
     """Return the evaluation suite ID"""
-    return retrieve_value(fp, "experiment", "suite_id")
+    eval_suite_id = retrieve_value(fp, "experiment", "suite_id")
+    print(f"[scrape_ini.py] Eval suite ID: {eval_suite_id}")
+    return eval_suite_id
 
 
 def find_ref_label(fp):
     """Return the reference label for plots"""
-    return retrieve_value(fp, "reference", "label_for_plots")
+    ref_label = retrieve_value(fp, "reference", "label_for_plots")
+    print(f"[scrape_ini.py] Ref label: {ref_label}")
+    return ref_label
 
 
 def find_eval_label(fp):
     """Return the evaluation label for plots"""
-    return retrieve_value(fp, "experiment", "label_for_plots")
+    eval_label = retrieve_value(fp, "experiment", "label_for_plots")
+    print(f"[scrape_ini.py] Eval label: {eval_label}")
+    return eval_label
