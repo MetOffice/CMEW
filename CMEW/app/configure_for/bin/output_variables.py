0c87e3edbd649d6e509599485f820be593200456
Naomi Parsons <naomi.parsons@metoffice.gov.uk>
2026-06-22 09:12:53 +0100

    Docstring changes

--- CMEW/app/configure_for/bin/output_variables.py
+++ CMEW/app/configure_for/bin/output_variables.py
@@ -20,6 +20,63 @@ def parse_variables_from_recipe(recipe_path):
     This function will first look to see if the variable's "short_name"
     key is present and use it if so or return a higher level of key if not.
 
+    * Read the ESMValTool recipe YAML file from the provided ``recipe_path``
+    * For each diagnostic defined in the recipe, extract the variables required
+      for that diagnostic
+    * For each variable, extract the mip table name
+    * Output a newline-separated list of variables, with each line formatted
+      as ``<mip>/<variable>``
+
+    Recipe file snippet, format 1::
+
+        diagnostics:
+          <diagnostic_1>:
+            variables:
+              other_key_name_1:
+                short_name: <variable_1a>:
+                mip: <mip_1a>
+              other_key_name_2:
+                short_name: <variable_1b>:
+                mip: <mip_1b>
+          <diagnostic_2>:
+            variables:
+              other_key_name_3:
+                short_name: <variable_2a>:
+                mip: <mip_2a>
+              other_key_name_4:
+                short_name: <variable_2b>:
+                mip: <mip_2b>
+
+    Will be formatted as::
+
+        <mip_1a>/<variable_1a>
+        <mip_1b>/<variable_1b>
+        <mip_2a>/<variable_2a>
+        <mip_2b>/<variable_2b>
+
+    Recipe file snippet, format 2::
+
+        diagnostics:
+          <diagnostic_1>:
+            variables:
+              <variable_1a>:
+                mip: <mip_1a>
+              <variable_1b>:
+                mip: <mip_1b>
+          <diagnostic_2>:
+            variables:
+              <variable_2a>:
+                mip: <mip_2a>
+              <variable_2b>:
+                mip: <mip_2b>
+
+    Will also be formatted as::
+
+        <mip_1a>/<variable_1a>
+        <mip_1b>/<variable_1b>
+        <mip_2a>/<variable_2a>
+        <mip_2b>/<variable_2b>
+
     Parameters
     ----------
     recipe_path : str
