28e09ee6184d3fe6b7f24bfebfed1b29d38a7a89
Naomi Parsons <naomi.parsons@metoffice.gov.uk>
2026-07-15 15:19:10 +0100

    Reconfiguring fetch_recipe

--- CMEW/app/configure_for/bin/fetch_recipe.py
+++ CMEW/app/configure_for/bin/fetch_recipe.py
@@ -12,65 +12,79 @@ filename = os.path.basename(__file__)
 logger = logging.getLogger(filename)
 
 
-def retrieve_name_and_fp():
+def retrieve_name_and_fp(recipe_id, recipe_dict_fp):
     """
     Looks in recipe_paths.yml for an entry or constructs default values.
 
     Uses the environment variable CYLC_TASK_PARAM_recipe as a dict key.
 
+    Parameters
+    ----------
+    recipe_id : str
+        The short identifier of the recipe to retrieve,
+        as written in the `flow.cylc` file.
+    recipe_dict_fp : str
+        The filepath of the YAML file containing the name and location
+        within esmvaltool.recipes of the recipe to be fetched.
+
     Returns
     -------
     recipe_name: str
         The name of the recipe, usually of the form recipe_thing.yml
-    recipe_fp: str
-        The location of the recipe withing esmvaltool/recipes
+    recipe_internal_loc: str
+        The location of the recipe within esmvaltool/recipes
 
     """
-    # Look up the recipe and destination from the environment
-    recipe = os.environ["CYLC_TASK_PARAM_recipe"]
-    logger.info("Fetching recipe %s", recipe)
+    logger.info("Fetching recipe %s", recipe_id)
 
     # Load the yaml config file from ../etc
-    recipe_dict_fp = os.environ["RECIPE_DICT_PATH"]
-    logger.debug("Reading recipe dict from %s", recipe_dict_fp)
     with open(recipe_dict_fp, "r") as f:
         recipe_dict = yaml.safe_load(f)
     logger.debug("Recipe dict:\n%s", recipe_dict)
 
     # Read specific recipe names and filepaths from the yaml config file
-    if recipe in recipe_dict:
-        logger.debug("Using info from recipe dictionary for %s", recipe)
-        recipe_name = recipe_dict[recipe]["recipe_name"]
-        recipe_fp = recipe_dict[recipe]["recipe_fp"]
+    if recipe_id in recipe_dict:
+        logger.debug("Using info from recipe dictionary for %s", recipe_id)
+        recipe_name = recipe_dict[recipe_id]["recipe_name"]
+        recipe_internal_loc = recipe_dict[recipe_id]["recipe_internal_loc"]
 
     # Or use the defaults
     else:
-        logger.debug("Using default name and filepath for recipe %s", recipe)
-        recipe_name = f"recipe_{recipe}.yml"
-        recipe_fp = recipe_name
+        logger.debug(
+            "Using default name and filepath for recipe %s", recipe_id
+        )
+        recipe_name = f"recipe_{recipe_id}.yml"
+        recipe_internal_loc = recipe_name
 
-    return recipe_name, recipe_fp
+    return recipe_name, recipe_internal_loc
 
 
-def main():
-    """Fetch a recipe from ESMValTool and copy it to the recipe path."""
+def fetch_recipe(recipe_id, recipe_dict_fp, output_filepath):
+    """
+    Fetch a recipe from ESMValTool and copy it to the output filepath.
+
+    Parameters
+    ----------
+    recipe_id : str
+        The short identifier of the recipe to retrieve,
+        as written in the `flow.cylc` file.
+    recipe_dict_fp : str
+        The filepath of the YAML file containing the name and location
+        within esmvaltool.recipes of the recipe to be fetched.
+    output_filepath: str
+        The full path to the where the ESMValTool recipe will be written.
+    """
     # Find the full name and location within ESMValTool
-    recipe_name, recipe_fp = retrieve_name_and_fp()
-
-    # Look up final destination
-    destination_fp = os.environ["RECIPE_PATH"]
-    logger.info("Recipe will be written to %s", destination_fp)
+    recipe_name, recipe_internal_loc = retrieve_name_and_fp(
+        recipe_id, recipe_dict_fp
+    )
 
     # Build the command to fetch and move the recipe
     command = f"""
-    cmew-esmvaltool-env esmvaltool recipes get {recipe_fp}
-    mv {recipe_name} {destination_fp}
+    esmvaltool recipes get {recipe_internal_loc}
+    mv {recipe_name} {output_filepath}
     """
 
     # Run the command
     logging.info("Running command: %s", command)
     subprocess.run(command, shell=True)
-
-
-if __name__ == "__main__":
-    main()
