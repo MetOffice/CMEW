42812f368983f94fdc010b36552df326b546d1a8
Emma Hogan <ehogan@users.noreply.github.com>
2026-08-21 17:34:15 +0100

    #558: Refactor update_recipe_file.py, remove many non-Python files, simplify tests

--- CMEW/app/configure_for/bin/test_update_recipe_file.py
+++ CMEW/app/configure_for/bin/test_update_recipe_file.py
@@ -1,119 +1,43 @@
 # (C) Crown Copyright 2024-2026, Met Office.
 # The LICENSE.md file contains full licensing details.
 """
-Unit tests for update_recipe_file.py
-
-Test data files:
-/app/unittest/mock_data/original_recipe_radiation_budget.yml
-    input for test_return_blank_recipe
-    input for test_main
-/app/unittest/kgo/blank_recipe_radiation_budget.yml
-    kgo for test_return_blank_recipe
-/app/unittest/mock_data/updated_recipe_radiation_budget.yml
-    input for test_add_extra_datasets
-/app/unittest/mock_data/cmip6_datasets.yml
-    input for test_add_extra_datasets
-/app/unittest/mock_data/model_runs.yml
-    input for test_main
-/app/unittest/kgo/extended_radiation_budget_recipe.yml
-    kgo for test_add_extra_datasets
-    kgo for test_main
+Unit tests for 'update_recipe_file.py'.
 """
 from update_recipe_file import (
-    return_blank_recipe,
-    add_extra_datasets,
+    remove_datasets_contents,
     remove_additional_datasets,
-    update_recipe_file,
+    construct_datasets_contents,
+    add_datasets_contents,
 )
-import shutil
-import yaml
-
 from configure_for_conftest import (
-    recipe_paths_yml_fp,
-    model_runs_yml_fp,
-    cmip6_datasets_yml_fp,
-    original_recipe_radiation_budget_fp,
-    recipe_with_additional_datasets_yml_fp,
-    updated_recipe_radiation_budget_yml_fp,
-    no_ds_in_recipe_fp,
-    recipe_additional_datasets_removed_yml_fp,
-    extended_radiation_budget_recipe_yml_fp,
+    datasets,
+    model_datasets,
+    cmip6_datasets,
+    recipe_with_datasets,
+    recipe_without_datasets,
+    recipe_without_additional_datasets,
 )
 
 
-def test_return_blank_recipe():
-    with open(str(no_ds_in_recipe_fp()), "r") as file_handle:
-        expected = yaml.safe_load(file_handle)
-    actual = return_blank_recipe(str(original_recipe_radiation_budget_fp()))
-    assert actual == expected
-
-
-def test_add_extra_datasets():
-    with open(
-        str(extended_radiation_budget_recipe_yml_fp()), "r"
-    ) as file_handle_1:
-        expected = yaml.safe_load(file_handle_1)
-
-    with open(
-        str(updated_recipe_radiation_budget_yml_fp()), "r"
-    ) as file_handle_2:
-        pre_recipe = yaml.safe_load(file_handle_2)
-
-    # Using str(filepath) here as update_recipe_file.py uses os, not pathlib
-    actual = add_extra_datasets(pre_recipe, str(cmip6_datasets_yml_fp()))
+def test_remove_datasets_contents():
+    actual = remove_datasets_contents(recipe_with_datasets())
+    expected = recipe_without_datasets()
     assert actual == expected
 
 
 def test_remove_additional_datasets():
-    recipe_id = "mock_entry"
-    with open(
-        str(recipe_additional_datasets_removed_yml_fp()), "r"
-    ) as file_handle_1:
-        expected = yaml.safe_load(file_handle_1)
-
-    with open(
-        str(recipe_with_additional_datasets_yml_fp()), "r"
-    ) as file_handle_2:
-        pre_recipe = yaml.safe_load(file_handle_2)
-
-    # Using str(filepath) here as update_recipe_file.py uses os, not pathlib
-    actual = remove_additional_datasets(
-        pre_recipe, recipe_id, str(recipe_paths_yml_fp())
-    )
+    actual = remove_additional_datasets(recipe_with_datasets())
+    expected = recipe_without_additional_datasets()
     assert actual == expected
 
 
-def test_update_recipe_file(tmp_path):
-    """update_recipe_file() should overwrite the recipe in place."""
-    # Copy the original recipe to a tmp_path location to allow it to be
-    # overwritten.
-    path_to_temp_recipe = tmp_path / "tmp_recipe.yml"
-    shutil.copy(
-        str(original_recipe_radiation_budget_fp()), path_to_temp_recipe
-    )
-    recipe_path = str(path_to_temp_recipe)
+def test_construct_datasets_contents():
+    actual = construct_datasets_contents([model_datasets(), cmip6_datasets()])
+    expected = datasets()
+    assert actual == expected
 
-    # These are used to check with additional datasets are removed
-    recipe_id = "radiation_budget"
 
-    update_recipe_file(
-        recipe_path,
-        str(model_runs_yml_fp()),
-        str(cmip6_datasets_yml_fp()),
-        recipe_id,
-        str(recipe_paths_yml_fp()),
-    )
-
-    with open(path_to_temp_recipe, "r") as file_handle_1:
-        actual_lines = file_handle_1.readlines()
-
-    with open(
-        str(extended_radiation_budget_recipe_yml_fp()), "r"
-    ) as file_handle_2:
-        kgo_with_comment = file_handle_2.readlines()
-
-    # Remove the five comment lines at the top of
-    # 'updated_recipe_radiation_budget.yml'.
-    kgo_without_comment = kgo_with_comment[5:]
-
-    assert actual_lines == kgo_without_comment
+def test_add_datasets_contents():
+    actual = add_datasets_contents(recipe_without_datasets(), datasets())
+    expected = recipe_with_datasets()
+    assert actual == expected
