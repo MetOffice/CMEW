ba45c8a857d4a48ab5ec46b7fe23346965efbf95
Naomi Parsons <naomi.parsons@metoffice.gov.uk>
2026-09-10 16:41:53 +0100

    Remove test of file writing

--- CMEW/app/configure_standardise/bin/test_create_variables_file.py
+++ CMEW/app/configure_standardise/bin/test_create_variables_file.py
@@ -15,9 +15,7 @@ Test data files:
 from create_variables_file import (
     combine_variable_lists,
     add_stream_to_variables,
-    write_variables,
 )
-import tempfile
 from configure_standardise_conftest import (
     mock_data_dir,
     variables_txt_fp,
@@ -70,33 +68,3 @@ def test_add_stream_to_variables():
         expected = file.read().splitlines()
 
     assert actual == expected
-
-
-def test_write_variables():
-    input = [
-        "Amon/hfls:apm",
-        "Amon/hfss:apm",
-        "Amon/rlds:apm",
-        "Emon/rls:apm",
-        "Amon/rlut:apm",
-        "Amon/rlutcs:apm",
-        "Amon/rsds:apm",
-        "Amon/rsdt:apm",
-        "Emon/rss:apm",
-        "Amon/rsut:apm",
-        "Amon/rsutcs:apm",
-        "Amon/tas:apm",
-        "SImon/siconc:inm",
-    ]
-
-    # Write the test dictionary to a temporary file
-    with tempfile.NamedTemporaryFile() as tmp:
-        write_variables(input, tmp.name)
-        tmp.seek(0)
-        actual = tmp.read().decode("utf-8")  # decode bytes to string
-
-    # Load the expected list
-    with open(str(variables_txt_fp()), "r") as file_handle:
-        expected = file_handle.read()
-
-    assert expected == actual
